020c145ade7eccb68de08234b9f2362eec906edf
Andres Valle <andresvalle.isi@gmail.com>
2014-02-09 19:54:23 -0300

    Added passenger search at new reservation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -70,7 +70,7 @@ module ApplicationHelper
 
   # [ { id: 4, dni: 12345678, full_name: 'pepe argento' ]
   def passengers_as_json(passengers)
-    passengers.map{ |p| { passenger_id: p.id, dni: p.dni, passenger_denomination: p.dni + ' (' + p.name + ' ' + p.lastname + ')' } }.to_json
+    passengers.map{ |p| { passenger_id: p.id, dni: p.dni, passenger_denomination: p.name + ' ' + p.lastname + ' (' + p.dni + ')' } }.to_json
   end
 
 end
