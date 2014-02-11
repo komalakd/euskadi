00b5702af06f55673e5e55d445193d0a8d18c224
Andrés Valle <andresvalle.isi@gmail.com>
2014-02-11 17:14:16 -0300

    Added test user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :login, :lastname, :name, :email, :password, presence: true
-  before_save { generate_token(:remember_token) }
+  # before_save { generate_token(:remember_token) }
 
 end
