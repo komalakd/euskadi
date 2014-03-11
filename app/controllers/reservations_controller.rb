c62aa78406140aa918d152203e21dfef04ac261c
Andres Valle <andresvalle.isi@gmail.com>
2014-03-11 02:02:57 -0300

    Fixed no-passengers text alert.

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -25,7 +25,7 @@ class ReservationsController < ApplicationController
     @passengers = Passenger.all
     if !(@passengers.size > 0)
       respond_to do |format|
-        format.html { redirect_to reservations_path, alert: 'Para registran una nueva reserva debe cargar al menos un pasajero.' }
+        format.html { redirect_to reservations_path, alert: 'Para registrar una nueva reserva debe cargar al menos un pasajero.' }
       end
     end
     @reservation = Reservation.new
