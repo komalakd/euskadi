0e2ccab070902ed1205848b01c54a1c9547fcc12
Andres Valle <andresvalle.isi@gmail.com>
2014-03-19 01:11:58 -0300

    Added DNI validation.

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -4,4 +4,5 @@ class Passenger < ActiveRecord::Base
   has_many :reservation_rooms, through: :room_passengers
   validates :dni, :lastname, :name, :phone_number, presence: true
   validates_uniqueness_of :dni
+  validates_numericality_of :dni, greater_than: 0
 end
