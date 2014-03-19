7c06da44bab9717938a5aeafade6fb90f8679335
Andres Valle <andresvalle.isi@gmail.com>
2014-03-19 00:14:29 -0300

    Added group amount validation.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,8 @@
 class Group < ActiveRecord::Base
     has_many :rooms
     has_many :reservations, as: :reservation_item
-    validates :name, presence: true
+    validates :name, :amount, presence: true
+    validates_numericality_of :amount, greater_than: 0
 end
 
 public
