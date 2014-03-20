75516a20e1ffab3579bd3e23f4ac5bfd3f182fef
Andres Valle <andresvalle.isi@gmail.com>
2014-03-19 22:02:38 -0300

    Added user validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   validates :login, :lastname, :name, :email, presence: true
   before_save :create_remember_token
+  validates_uniqueness_of :login
+  validates_uniqueness_of :email
 
   has_secure_password
   validates_presence_of :password, :on => :create
